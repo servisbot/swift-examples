b8198461c4c7e3188fd620e469a63568b937d56f
Ty Rollin <ty.rollin@reactsoftwareinc.com>
2023-03-03 11:58:30 -0500

    init

---
+++ ServisBOT Swift Examples/ServisBOT Swift Examples/ViewController.swift
@@ -0,0 +1,63 @@
+//
+//  ViewController.swift
+//  ServisBOT Swift Examples
+//
+//  Created by Ty Rollin on 3/3/23.
+//
+
+import UIKit
+import WebKit
+import ServisBOTSDK
+
+class ViewController: UIViewController, WKUIDelegate, WKNavigationDelegate {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        showMessenger()
+    }
+
+    private func showMessenger() {
+        let organization = "netscapestg"
+        let endpoint = "netscapestg-AskBot"
+        let sbRegion = "us-1"
+        
+        let basicConfig:[String:Any] = [
+            "organization": organization,
+            "endpoint": endpoint,
+            "sbRegion": sbRegion,
+        ]
+
+        let sbMessenger = Messenger(config: basicConfig, resetAtStart: false)
+                
+        do {
+            let sbView: WKWebView = try sbMessenger.load()
+            sbView.navigationDelegate = self
+            self.view.addSubview(sbView)
+            setLayoutDetails(webView: sbView)
+        } catch {
+            print("Messenger failed to load")
+        }
+    }
+    
+    func webView(_ webView: WKWebView, didFinish  navigation: WKNavigation!)
+    {
+        let url = webView.url?.absoluteString
+        print("---Loaded URL--->\(url!)") // here you are getting URL
+    }
+    
+    private func setLayoutDetails(webView: WKWebView) {
+        // Show web view on screen
+        webView.layer.cornerRadius = 20.0
+        webView.layer.masksToBounds = true
+        webView.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            webView.topAnchor.constraint(equalTo: self.view.layoutMarginsGuide.topAnchor, constant: 20.0),
+            webView.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant: 20.0),
+            webView.trailingAnchor.constraint(equalTo: self.view.trailingAnchor, constant: -20.0),
+            webView.bottomAnchor.constraint(equalTo: self.view.layoutMarginsGuide.bottomAnchor, constant: -20.0),
+        ])
+    }
+
+}
+
